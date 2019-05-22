64565b8efd70da86615c5505100d1d3924ad0ad3
alex <drunkod@gmail.com>
2019-05-22 11:44:51 +0500

    fix

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ module.exports = async function(context, req) {
   } else {
     context.res = {
       status: 400,
-      body:
-        "Please  jhbhjb pass a name on the query string or in  kjkjthe request body"
+      body: "Please the query string or in  kjkjthe request body"
     };
   }
 };
