22d54cc364777036a1d1e70071943e2aa1a77939
alex <drunkod@gmail.com>
2019-05-22 11:41:58 +0500

    hgvhvg

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ module.exports = async function(context, req) {
   } else {
     context.res = {
       status: 400,
-      body: "Please pass a name on the query string or in  kjkjthe request body"
+      body:
+        "Please  jhbhjb pass a name on the query string or in  kjkjthe request body"
     };
   }
 };
